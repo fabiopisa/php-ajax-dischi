c1e304f67dc04cdb61705adeea2e69133b64c808
fabiopisa <pisanellifabio@gmail.com>
2021-06-09 15:45:50 +0200

    commit esercizio finito

--- js/script.js
+++ js/script.js
@@ -2,17 +2,26 @@ const app = new Vue({
   el:'#app',
   data:{
     arrDisc:[],
+    arrGenres:[],
+    strValGenre:'all',
+    apiURL:'http://localhost/php-intro/php-ajax-dischi/api.php'
   },
   created(){
-    this.callApi('api.php')
+    this.callApi()
   },
   methods:{
-    callApi(url){
-      axios.get(url)
+    callApi(){
+      axios.get(this.apiURL,{
+        params:{
+          genre:this.strValGenre
+        }
+      })
       .then((resp)=>{
         console.log(resp);
-        this.arrDisc.push(resp.data);
+        this.arrDisc = (resp.data.albums);
+        this.arrGenres = (resp.data.genres);
         console.log(this.arrDisc);
+        console.log(this.arrGenres);
       })
       .catch((err)=>{
         console.log(err);
